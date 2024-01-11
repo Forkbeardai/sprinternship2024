955427b8154c1534f91aad58793da07a1c5bdd78
Aditri Gadigi <adi3.gadigi@gmail.com>
2024-01-11 10:25:16 -0500

    aditri's updated file

--- regex_project.py
+++ regex_project.py
@@ -3,20 +3,20 @@ import argparse
 import snowflake.connector
 
 class RegexGenerator:
-    def __init__(self, regex, count=1):
-        self.regex = regex
-        self.count = count
+    def __init__(self, regex_count):
+        self.regex_count = regex_count
 
     def generate_data(self):
-        for i in range(self.count):
-            generated_data = exrex.getone(self.regex)
-            print(generated_data)
+        for regex, count in self.regex_count_pairs:
+            print(f"Generating {count} data items for regex: {regex}")
+            for i in range(count):
+                generated_data = exrex.getone(regex)
+                print(generated_data)
 
 class CommandLineParser:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description = 'Generate data based on a given regular expression.')
-        self.parser.add_argument('regex', type = str, help = 'RegEx')
-        self.parser.add_argument('--count', type = int, default = 1, help = 'Number of data items to generate (default is 1)')
+        self.parser.add_argument('regex_count', nargs = '+', type = lambda s: s.split(':'), help = 'List of regex:count pairs, each separated by space')
         self.parser.add_argument('--snowflake-user', type = str, default = 'default', help = 'Snowflake username (not required)')
         self.parser.add_argument('--snowflake-password', type = str, default = 'default', help = 'Snowflake password (not required)')
         self.parser.add_argument('--snowflake-account', type = str, default = 'default', help = 'Snowflake account (not required)')
@@ -27,13 +27,13 @@ class CommandLineParser:
     def parse_args(self):
         return self.parser.parse_args()
 
-##class SnowflakeConnector:
-
 def main():
     parser = CommandLineParser()
     args = parser.parse_args()
 
-    generator = RegexGenerator(args.regex, args.count)
+    regex_count_pairs = [(regex, int(count)) for regex, count in args.regex_count_pairs]
+
+    generator = RegexGenerator(regex_count_pairs)
     generator.generate_data()
 
 main()
