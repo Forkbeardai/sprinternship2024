0d6fb0966d3379bdd8dc8460f9fb45c25832d601
Aditri Gadigi <adi3.gadigi@gmail.com>
2024-01-22 09:45:23 -0500

    added error messages

--- regex_project.py
+++ regex_project.py
@@ -6,14 +6,14 @@ from itertools import zip_longest
 import random
 
 
-# Class for managing Snowflake connection
+#Class for managing Snowflake connection
 class SnowflakeConnector:
     def __init__(self, snowflake_config):
         self.snowflake_config = snowflake_config
         self.connection = None
 
     def connect(self):
-        # Establish a connection to Snowflake using provided configuration
+        #Establish a connection to Snowflake using credentials
         self.connection = snowflake.connector.connect(
             user  = self.snowflake_config['user'],
             password = self.snowflake_config['password'],
@@ -24,14 +24,14 @@ class SnowflakeConnector:
         )
 
     def close(self):
-        # Close the Snowflake connection if it's open
+        #Close  Snowflake connection
         if self.connection:
             self.connection.close()
 
 
 
 
-# Class for generating data based on regex patterns
+#Class for generating data based on regex patterns
 class DataGenerator:
     def __init__(self, regexes, count, invalid_regexes, invalid_data_percentages):
         self.regexes = regexes
@@ -40,11 +40,11 @@ class DataGenerator:
         self.invalid_data_percentages = invalid_data_percentages
 
     def generate_data(self):
-        data = []  #Initialize an empty list to store generated data for each regex.
+        data = []  
 
         for regex, invalid_regex, invalid_percentage in zip(self.regexes, self.invalid_regexes, self.invalid_data_percentages):
             valid_count = int(int(self.count) * (1 - float(invalid_percentage) / 100))
-            invalid_count = int(int(self.count) * float(invalid_percentage) / 100) #how many invalid data we should generate
+            invalid_count = int(int(self.count) * float(invalid_percentage) / 100) #How many invalid data we should generate
 
             print(f"num of valid count is {valid_count}, num of invalid count is {invalid_count}")
             #Generate valid data
@@ -64,7 +64,7 @@ class DataGenerator:
 
 
 
-# Class for Snowflake database operations
+#Class for Snowflake database operations
 class SnowflakeOperations:
     def __init__(self, snowflake_connector, table_name, column_names):
         self.snowflake_connector = snowflake_connector
@@ -72,7 +72,7 @@ class SnowflakeOperations:
         self.column_names = column_names
 
     def create_table(self):
-        # Create a table in Snowflake with specified columns
+        #Create a table in Snowflake with specified columns
         connection = self.snowflake_connector.connection
         cursor = connection.cursor()
         cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name} (id INTEGER AUTOINCREMENT PRIMARY KEY)")
@@ -97,11 +97,9 @@ class SnowflakeOperations:
             placeholders = ', '.join(['%s' for _ in self.column_names])
             values_placeholder = ', '.join([f"({placeholders})" for _ in batch])
             sql = f"INSERT INTO {self.table_name} ({columns}) VALUES {values_placeholder}"
-
-            #Flatten the batch list of tuples for execution
             flattened_values = [item for sublist in batch for item in sublist]
 
-            #Execute the SQL statement
+            #Execute SQL statement
             cursor.execute(sql, flattened_values)
 
         connection.commit()
@@ -117,23 +115,28 @@ class CommandLineParser:
         self.parser.add_argument('-d', '--database', type = str, required = True, help = 'Snowflake database')
         self.parser.add_argument('-s', '--schema', type = str, required = True, help = 'Snowflake schema')
         self.parser.add_argument('-t', '--table_name', type = str, required = True, help = 'Snowflake table name')
-        self.parser.add_argument('-c', '--count', type = str, help = 'Amount of data to be generated for each regex')
+        self.parser.add_argument('-c', '--count', type = str, help = 'Amount of data to be generated for each regex (must be positive ot 0)')
         self.parser.add_argument('-r', '--regex', type = str, action = 'append', help = 'Regex')
         self.parser.add_argument('-n', '--column_name', type = str, action = 'append', help = 'Column name for corresponding regex')
         self.parser.add_argument('-i', '--invalid_regexes', type = str, action = 'append', help = 'Invalid regex for corresponding regex')
-        self.parser.add_argument('-pct', '--invalid_data_percentages', type = str, action = 'append', help = 'Invalid data percentage for corresponding regex')
+        self.parser.add_argument('-pct', '--invalid_data_percentages', type = str, action = 'append', help = 'Invalid data percentage for corresponding regex (must be between 0 and 100).')
 
 
     def parse_args(self):
         args = self.parser.parse_args()
         if args.password == 'default':
-            args.password = getpass.getpass('Enter your Snowflake password(hidden): ')
+            args.password = getpass.getpass('Enter your Snowflake password (hidden): ')
+        if int(args.count) < 0:
+            raise Exception("Count cannot be negative.")
+        for element in args.invalid_data_percentages:
+            if int(element) < 0 or int(element) > 100:
+                raise Exception("Percentage must be between 0 and 100.")
         return args
 
 
 def main():
     parser = CommandLineParser()
-    args = parser.parse_args() #parse the command user inputted, now can access the specific data by caling args.xxx
+    args = parser.parse_args() #Parse the command user inputted, now can access the specific data by caling args.xxx
 
     snowflake_config = {
         'user': args.user,
@@ -147,7 +150,7 @@ def main():
 
     try:
         snowflake_connector = SnowflakeConnector(snowflake_config)
-        snowflake_connector.connect() #connect to snowflake
+        snowflake_connector.connect() #Connect to snowflake
 
         data_generator = DataGenerator(args.regex, args.count, args.invalid_regexes, args.invalid_data_percentages)
         generated_data = data_generator.generate_data()
@@ -155,8 +158,8 @@ def main():
         snowflake_operations = SnowflakeOperations(snowflake_connector, args.table_name, args.column_name)
         snowflake_operations.create_table()
 
-        # Using batch insert
-        snowflake_operations.insert_data_in_batches(generated_data, batch_size=100)  # You can adjust the batch size
+        #Using batch insert
+        snowflake_operations.insert_data_in_batches(generated_data, batch_size=100)  #You can adjust the batch size
 
     except Exception as e:
         print(f"An error occurred: {e}")
