25fb00404feb7b2c4d1a964a43d372d9a107a442
jamielinzheng <Igotanicejam@gmail.com>
2024-01-22 10:17:18 -0500

    Jamie's data generator test

--- data_generator_class_test.py
+++ data_generator_class_test.py
@@ -19,27 +19,6 @@ class TestDataGenerator(unittest.TestCase):
             for datum in data:
                 self.assertEqual(len(datum), self.count)
 
-        def test_generate_data_validity(self):
-            data = self.data_gen.generate_data()
-            valid_data_regex = re.compile(self.regexes[0])
-            for datum in data[0][:int(self.count * (1 - self.invalid_data_percentages[0] / 100))]:
-                self.assertTrue(valid_data_regex.fullmatch(datum))
-
-        def test_invalid_percentage_handling(self):
-            regexes = ['\d{3}']
-            
-            # Test with a percentage greater than 100
-            invalid_regexes_over = ['\D+']
-            invalid_data_percentages_over = [110]
-            with self.assertRaises(ValueError):
-                DataGenerator(regexes, 10, invalid_regexes_over, invalid_data_percentages_over).generate_data()
-
-            # Test with a percentage less than 0
-            invalid_regexes_under = ['\D+']
-            invalid_data_percentages_under = [-10]
-            with self.assertRaises(ValueError):
-                DataGenerator(regexes, 10, invalid_regexes_under, invalid_data_percentages_under).generate_data()
-
 
 if __name__ == '__main__':
     unittest.main()
